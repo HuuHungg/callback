ba7d5a71f0f1119f2a935be7ab44c2f18d021ffb
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2022-12-14 16:11:54 +0700

    add file callback

---
+++ calback.js
@@ -0,0 +1,30 @@
+Array.prototype.map2 = function (callback) {
+    let output = []
+    let arrLength = this.length
+    for(let i = 0; i < arrLength; i++) {
+       let result =  callback(this[i])
+       output.push(result)
+    }
+
+    return output
+}
+
+
+
+let course = [
+    'Javascript', 
+    'PHP',
+    'Ruby'
+]
+
+
+let htmls = course.map2((course)=> {
+    return `<h2 ${course} ></h2>`
+})
+
+console.log(htmls.join(''))
+
+// let courses = course.map( (course) => {
+//     return `<h2> ${course}</h2>`
+// })
+// console.log(courses)
