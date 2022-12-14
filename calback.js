a09962f7069f964142fadc3cfaecdfc4e662bcc6
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2022-12-14 16:38:22 +0700

    add all file

--- calback.js
+++ calback.js
@@ -1,33 +1,56 @@
-Array.prototype.map2 = function (callback) {
-    let output = []
-    let arrLength = this.length
-    for(let i = 0; i < arrLength; i++) {
-       let result =  callback(this[i])
-       output.push(result)
+// Array.prototype.map2 = function (callback) {
+//     let output = []
+//     let arrLength = this.length
+//     for(let i = 0; i < arrLength; i++) {
+//        let result =  callback(this[i])
+//        output.push(result)
+//     }
+
+//     return output
+// }
+
+
+
+// let course = [
+//     'Javascript', 
+//     'PHP',
+//     'Ruby'
+// ]
+
+
+// let htmls = course.map2((course)=> {
+//     return `<h2 ${course} ></h2>`
+// })
+
+// console.log(htmls.join(''))
+
+// // let courses = course.map( (course) => {
+// //     return `<h2> ${course}</h2>`
+// // })
+// // console.log(courses)
+
+
+// console.log('Huu Hung Nguyen')
+
+
+let courses = [
+    {
+       name: 'Java',
+       course: 200,
+    },
+    {
+        name: 'PHP',
+        course: 20
     }
-
-    return output
-}
-
-
-
-let course = [
-    'Javascript', 
-    'PHP',
-    'Ruby'
 ]
 
+let courseName = courses.map((course,index) => {
+    return {
+        name: `Khoa hoc ${course.name}`,
+        course: `Khoa hoc co gia la ${course.course}`,
+        index: index 
 
-let htmls = course.map2((course)=> {
-    return `<h2 ${course} ></h2>`
+    }
 })
 
-console.log(htmls.join(''))
-
-// let courses = course.map( (course) => {
-//     return `<h2> ${course}</h2>`
-// })
-// console.log(courses)
-
-
-console.log('Huu Hung Nguyen')
+console.log(courseName)
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-function myFunction(param) {
-    param("Hoc Lap Trinh")
+Array.prototype.map2 = function (callback) {
+    let output = []
+    let arrLength = this.length
+    for(let i = 0; i < arrLength; i++) {
+       let result =  callback(this[i])
+       output.push(result)
+    }
+
+    return output
 }
 
-function myCallback(Value) {
-    console.log('Type of Value: ', Value)
-}
 
-myFunction(myCallback)
+
+let course = [
+    'Javascript', 
+    'PHP',
+    'Ruby'
+]
+
+
+let htmls = course.map2((course)=> {
+    return `<h2 ${course} ></h2>`
+})
+
+console.log(htmls.join(''))
+
+// let courses = course.map( (course) => {
+//     return `<h2> ${course}</h2>`
+// })
+// console.log(courses)
